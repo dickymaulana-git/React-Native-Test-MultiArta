b2e880a382ccf6a40c420246c8240d998fa328ed
dickym <dicky.maulana@live.com>
2021-06-26 08:23:02 +0700

    update-debug-apk

--- RnMultiArta/src/components/SearchBar.js
+++ RnMultiArta/src/components/SearchBar.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput, Platform} from 'react-native';
+import {StyleSheet, View, TextInput, Platform} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import color from '../../src/config/Colors';
-import {ForceTouchGestureHandler} from 'react-native-gesture-handler';
 
 export default function SearchBar(props) {
   return (
     <View style={styles.search}>
-      <Icon name="magnify" size={25} />
+      <Icon style={Platform.OS == 'android' && styles.iconSearch} name="magnify" size={25} />
       <TextInput
         autoCorrect={false}
         autoFocus={true}
@@ -33,10 +32,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: Platform.OS === 'android' ? wp(0) : wp(1),
     marginBottom: hp(2),
+    alignItems:'center'
   },
   input: {
     width: wp(79),
     maxWidth: wp(79),
     marginLeft: wp(2),
   },
+  iconSearch:{
+    marginLeft:wp(2)
+  }
 });
